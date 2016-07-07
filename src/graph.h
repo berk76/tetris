0c93ad7cc4c54883ddb0011fb7fef7cbbbee2256
jaroslav_b <jaroslav_b@localhost>
2016-07-07 14:14:13 +0200

    Refactoring of colors

--- src/graph.h
+++ src/graph.h
@@ -9,10 +9,17 @@
 #ifndef _GRAPH_
 #define _GRAPH_
 
+typedef struct {
+        HPEN hPen;
+        HBRUSH hBrush;
+} COLOR_T;
+
+#define G_COLORS_SIZE 8
 
 extern void g_draw_mesh(HDC hdc, TETRIS_T *tetris, RECT client);
 extern void g_put_mesh_pixel(HDC hdc, TETRIS_T *tetris, int x, int y, int color);
 extern void g_empty_mesh_pixel(HDC hdc, TETRIS_T *tetris, int x, int y);
+extern void g_set_colors(COLOR_T *colors);
 
 
 #endif
